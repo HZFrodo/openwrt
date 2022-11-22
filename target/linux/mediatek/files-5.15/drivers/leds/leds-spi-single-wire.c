225ee6e99c3ddcfe1410b3e7b93d057978f8a350
ROCK.XU <rock.xu@nio.com>
2022-11-22 10:55:18 +0800

    spi_ws2812b
    Signed-off-by: ROCK.XU <rock.xu@nio.com>

--- target/linux/mediatek/files-5.15/drivers/leds/leds-spi-single-wire.c
+++ target/linux/mediatek/files-5.15/drivers/leds/leds-spi-single-wire.c
@@ -6,78 +6,130 @@
 #include <linux/of_device.h>
 #include <linux/spi/spi.h>
 #include <linux/mutex.h>
-#include <uapi/linux/uleds.h>
+#include <linux/led-class-multicolor.h>
 
-#define LED_COLOR_MAX 5
-
-static unsigned char *led_color_table[LED_COLOR_MAX + 1] = {
-	"\x92\x49\x24\x92\x49\x24\x92\x49\x24", /* LED OFF */
-	"\xdb\x6d\x36\x92\x49\x24\x92\x49\x24", /* LED RED */
-	"\x92\x49\x24\x92\x49\x24\xdb\x6d\x36", /* LED BLUE */
-	"\xdb\x6d\x36\x9a\x49\x34\x92\x49\x24", /* LED YELLOW */
-	"\xda\x4d\x36\xdb\x6d\x36\xda\x49\x24", /* LED WHITE */
-	"\x92\x49\x24\xdb\x6d\x36\x92\x49\x24", /* LED GREEN */
+/* encoded byte tables
+ * we present each bit as 3 bits (oversampling by a factor of 3)
+ *   zero is represented as 0b100
+ *   one is represented as 0b110
+ * so each byte is actually represented as 3 bytes
+ *
+ * the following are tables to avoid repeated computations
+ * and are sent via spi as high, medium, low at 3* required HZ
+ */
+const char byte2encoding_h[/* (value >> 5) & 0x07 */] = {
+	0x92,
+	0x93,
+	0x9a,
+	0x9b,
+	0xd2,
+	0xd3,
+	0xda,
+	0xdb
 };
 
-struct spi_single_wire_chipdef {
+const char byte2encoding_m[/* (value >> 3) & 0x03 */] = {
+	0x49,
+	0x4d,
+	0x69,
+	0x6d
+};
+
+const char byte2encoding_l[/* value & 0x07 */] = {
+	0x24,
+	0x26,
+	0x34,
+	0x36,
+	0xa4,
+	0xa6,
+	0xb4,
+	0xb6
+};
+
+struct ws2812b_encoding {
+	u8 h, m, l;
+};
+
+struct ws2812b_pixel {
+	struct ws2812b_encoding r, g, b;
+};
+
+struct spi_ws2812b_chipdef {
 	unsigned char chip_num;
 	unsigned char led_ctrl_cmd_len;
+	unsigned char led_num_channels;
 };
 
-struct spi_single_wire_led {
-	struct led_classdev ldev;
-	struct spi_device *spi;
-	char name[LED_MAX_NAME_SIZE];
-	struct mutex mutex;
-	const struct spi_single_wire_chipdef *cdef;
-	unsigned char *led_ctrl_cmd;
-	unsigned char chip_id;
-	unsigned char color_id;
-};
-
-static struct spi_single_wire_chipdef miwifi_hm0807a_led = {
+static struct spi_ws2812b_chipdef miwifi_ws2812b_spi_led = {
 	.chip_num = 2, /* Redmi AX6000 has 2 LED chips */
+	.led_num_channels = 3,
 	.led_ctrl_cmd_len = 9, /* 9 bytes per LED chip */
 };
 
+struct spi_ws2812b_led {
+	struct led_classdev_mc mc_ldev;
+	struct spi_device *spi;
+	struct mutex mutex;
+	const struct spi_ws2812b_chipdef *cdef;
+	unsigned char *led_ctrl_cmd;
+	unsigned char chip_id;
+};
+
+static inline void ws2812b_set_encoded_pixel(struct ws2812b_encoding *enc, u8 val)
+{
+	enc->h = byte2encoding_h[(val >> 5) & 0x07];
+	enc->m = byte2encoding_m[(val >> 3) & 0x03];
+	enc->l = byte2encoding_l[(val >> 0) & 0x07];
+}
+
+static void ws2812b_set_pixel_value(struct ws2812b_pixel *spix, unsigned char r, unsigned char g, unsigned char b)
+{
+	/* assign the encoded values */
+	ws2812b_set_encoded_pixel(&spix->g, g);
+	ws2812b_set_encoded_pixel(&spix->r, r);
+	ws2812b_set_encoded_pixel(&spix->b, b);
+}
+
 static const struct spi_device_id spi_ids[] = {
-	{ .name = "HM0807A" },
+	{ .name = "ws2812b-spi", },
 	{},
 };
 
-static const struct of_device_id spi_single_wire_dt_ids[] = {
-	{ .compatible = "xiaomi,HM0807A", .data = &miwifi_hm0807a_led },
+static const struct of_device_id spi_ws2812b_dt_ids[] = {
+	{ .compatible = "xiaomi,ws2812b-spi", .data = &miwifi_ws2812b_spi_led, },
 	{},
 };
 
 MODULE_DEVICE_TABLE(of, spi_single_wire_dt_ids);
 
-static int spi_brightness_set_blocking(struct led_classdev *dev,
-					    enum led_brightness brightness)
+static int ws2812b_rgb_led_brightness_set_blocking(struct led_classdev *cdev, enum led_brightness brightness)
 {
-	struct spi_single_wire_led *led = container_of(dev, struct spi_single_wire_led, ldev);
+	struct led_classdev_mc *mc_ldev = lcdev_to_mccdev(cdev);
+	struct spi_ws2812b_led *led = container_of(mc_ldev, struct spi_ws2812b_led, mc_ldev);
+	struct ws2812b_pixel spix;
 	unsigned char *ctrl_cmd_head;
 	unsigned char *ctrl_cmd_payload;
 	unsigned char ctrl_cmd_len = led->cdef->chip_num * led->cdef->led_ctrl_cmd_len;
 	int ret = 0;
 
-	ctrl_cmd_head = kzalloc(ctrl_cmd_len + 2, GFP_KERNEL); /* 2 bytes pad is required */
+	ctrl_cmd_head = kzalloc(ctrl_cmd_len + 2, GFP_KERNEL); /* 2 bytes pad to make waveform more stable */
 	if (!ctrl_cmd_head)
 		return -ENOMEM;
 
 	mutex_lock(&led->mutex);
+
+	led_mc_calc_color_components(mc_ldev, brightness);
+	ws2812b_set_pixel_value(&spix, mc_ldev->subled_info[0].brightness,
+		mc_ldev->subled_info[1].brightness, mc_ldev->subled_info[2].brightness);
+
 	ctrl_cmd_payload = ctrl_cmd_head + 1; /* skip the SOF byte */
+	memcpy(led->led_ctrl_cmd, &spix, sizeof(spix)); /* save the current color */
 
-	if (brightness == LED_OFF) {
-		memcpy(led->led_ctrl_cmd, led_color_table[0], led->cdef->led_ctrl_cmd_len);
-	} else {
-		memcpy(led->led_ctrl_cmd, led_color_table[led->color_id + 1], led->cdef->led_ctrl_cmd_len);
-	}
-
+	/* now we output the color data via spi */
 	memcpy(ctrl_cmd_payload, led->led_ctrl_cmd - led->cdef->led_ctrl_cmd_len * led->chip_id,
 		led->cdef->led_ctrl_cmd_len * led->cdef->chip_num);
-
 	ret = spi_write(led->spi, ctrl_cmd_head, ctrl_cmd_len + 2); /* 2 bytes pad */
+
 	mutex_unlock(&led->mutex);
 
 	kfree(ctrl_cmd_head);
@@ -85,21 +137,22 @@ static int spi_brightness_set_blocking(struct led_classdev *dev,
 	return ret;
 }
 
-static int spi_single_wire_probe(struct spi_device *spi)
+static int spi_ws2812b_probe(struct spi_device *spi)
 {
-	struct spi_single_wire_led *led;
-	const struct spi_single_wire_chipdef *cdef;
+	struct spi_ws2812b_led *led;
+	const struct spi_ws2812b_chipdef *cdef;
 	struct device *dev = &spi->dev;
 	struct device_node *np = dev_of_node(dev);
 	struct device_node *chip_node;
-	struct device_node *color_node;
-	const char *color_name = "";
 	unsigned char *chip_cmd_buffer;
 	const char *state;
 	unsigned char cur_chip_id = 0;
-	unsigned char cur_color_id = 0;
-	int ret;
+	int ret = 0;
 	int chip_count;
+
+	struct led_classdev *cdev;
+
+	struct led_init_data init_data = {};
 
 	cdef = device_get_match_data(dev);
 	if (!cdef)
@@ -115,69 +168,72 @@ static int spi_single_wire_probe(struct spi_device *spi)
 		return -ENOMEM;
 
 	for_each_available_child_of_node(np, chip_node) {
-		for_each_available_child_of_node(chip_node, color_node) {
-			led = devm_kzalloc(dev, sizeof(*led), GFP_KERNEL);
-			if (!led)
-				return -ENOMEM;
+		led = devm_kzalloc(dev, sizeof(*led), GFP_KERNEL);
+		if (!led)
+			return -ENOMEM;
 
-			/* all LEDs of one chip share one cmd_buffer */
-			led->led_ctrl_cmd = chip_cmd_buffer + cdef->led_ctrl_cmd_len * cur_chip_id;
+		/* all LEDs of one chip share one cmd_buffer */
+		led->led_ctrl_cmd = chip_cmd_buffer + cdef->led_ctrl_cmd_len * cur_chip_id;
 
-			of_property_read_string(color_node, "label", &color_name);
-			snprintf(led->name, sizeof(led->name), "%s", color_name);
-			led->spi = spi;
-			mutex_init(&led->mutex);
-			led->cdef = cdef;
-			led->chip_id = cur_chip_id;
-			led->color_id = cur_color_id;
-			led->ldev.name = led->name;
-			led->ldev.brightness = LED_OFF;
-			led->ldev.max_brightness = LED_FULL;
-			led->ldev.brightness_set_blocking = spi_brightness_set_blocking;
+		led->spi = spi;
+		mutex_init(&led->mutex);
+		led->cdef = cdef;
+		led->chip_id = cur_chip_id;
 
-			state = of_get_property(color_node, "default-state", NULL);
-			if (state) {
-				if (!strcmp(state, "on")) {
-					led->ldev.brightness = led->ldev.max_brightness;
-				}
-				/* default to LED_OFF already */
-			}
-			spi_brightness_set_blocking(&led->ldev, led->ldev.brightness);
+		led->mc_ldev.num_colors = cdef->led_num_channels;
+		init_data.fwnode = &chip_node->fwnode;
 
-			ret = devm_led_classdev_register(&spi->dev, &led->ldev);
-			if (ret) {
-				mutex_destroy(&led->mutex);
-				return ret;
-			}
-			spi_set_drvdata(spi, led);
-			cur_color_id++;
+		led->mc_ldev.subled_info = devm_kzalloc(dev, sizeof(struct mc_subled) * led->mc_ldev.num_colors, GFP_KERNEL);
+		if (!led->mc_ldev.subled_info)
+			return -ENOMEM;
+
+		led->mc_ldev.subled_info[0].color_index = LED_COLOR_ID_RED;
+		led->mc_ldev.subled_info[0].channel = 0;
+		led->mc_ldev.subled_info[1].color_index = LED_COLOR_ID_GREEN;
+		led->mc_ldev.subled_info[1].channel = 1;
+		led->mc_ldev.subled_info[2].color_index = LED_COLOR_ID_BLUE;
+		led->mc_ldev.subled_info[2].channel = 2;
+
+		cdev = &led->mc_ldev.led_cdev;
+		cdev->brightness = LED_OFF;
+		cdev->max_brightness = LED_FULL;
+		cdev->brightness_set_blocking = ws2812b_rgb_led_brightness_set_blocking;
+
+		state = of_get_property(chip_node, "default-state", NULL);
+		if (state) {
+			if (!strcmp(state, "on"))
+				cdev->brightness = cdev->max_brightness;
+			/* default to LED_OFF already */
 		}
+		ws2812b_rgb_led_brightness_set_blocking(&led->mc_ldev.led_cdev, cdev->brightness);
+
+		spi_set_drvdata(spi, led);
+
+		if ((ret = devm_led_classdev_multicolor_register_ext(&spi->dev, &led->mc_ldev, &init_data)) < 0) {
+			dev_err(dev, "Cannot register LED %pOF: %i\n", chip_node, ret);
+			return -ENOMEM;
+		} else {
+			dev_info(dev, "register LED %pOF OK\n", chip_node);
+		}
+
 		cur_chip_id++;
-		cur_color_id = 0;
 	}
 
 	return 0;
 }
 
-static int spi_single_wire_remove(struct spi_device *spi)
-{
-	/* module should never be removed from kernel */
-	return 0;
-}
-
-static struct spi_driver spi_single_wire_led_driver = {
-	.probe		= spi_single_wire_probe,
-	.remove		= spi_single_wire_remove,
+static struct spi_driver spi_ws2812b_led_driver = {
+	.probe		= spi_ws2812b_probe,
 	.driver = {
 		.name		= KBUILD_MODNAME,
-		.of_match_table	= spi_single_wire_dt_ids,
+		.of_match_table	= spi_ws2812b_dt_ids,
 	},
 	.id_table = spi_ids,
 };
 
-module_spi_driver(spi_single_wire_led_driver);
+module_spi_driver(spi_ws2812b_led_driver);
 
 MODULE_AUTHOR("Furong Xu <xfr@outlook.com>");
-MODULE_DESCRIPTION("single wire SPI LED driver for Xiaomi Redmi AX6000");
+MODULE_DESCRIPTION("WS2812B RGB LED driver via SPI");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("spi:leds-spi-single-wire");
+MODULE_ALIAS("spi:leds-spi-ws2812b");
